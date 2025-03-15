7f7fc1c11b6ebf44e3689d1e95e68980d270a656
Danil <danil.s174@mail.ru>
2025-03-15 13:18:38 +0500

    file adding

--- echoserver/echo/forms.py
+++ echoserver/echo/forms.py
@@ -3,32 +3,37 @@ from .models import Book, User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class BookForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].label = 'Название книги'
+        self.fields['author'].label = 'Автор книги'
+        self.fields['price'].label = 'Цена'
     class Meta:
         model = Book
         fields = ['title', 'author', 'price']
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
-    #
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.fields['username'].label = 'Имя пользователя'
-    #     self.fields['password1'].label = 'Пароль'
-    #     self.fields['password2'].label = 'Подтверждение пароля'
-    #     self.fields['username'].help_text = None
-    #     self.fields['password1'].help_text = None
-    #     self.fields['password2'].help_text = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Имя пользователя'
+        self.fields['password1'].label = 'Пароль'
+        self.fields['password2'].label = 'Подтверждение пароля'
+        self.fields['username'].help_text = None
+        self.fields['password1'].help_text = None
+        self.fields['password2'].help_text = None
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2', 'role']
 
 class LoginForm(AuthenticationForm):
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.fields['username'].label = 'Имя пользователя'
-    #     self.fields['password'].label = 'Пароль'
-    # class Meta:
-    #     model = User
-    #     fields = ['username', 'password']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Имя пользователя'
+        self.fields['password'].label = 'Пароль'
+    class Meta:
+        model = User
+        fields = ['username', 'password']
     pass
